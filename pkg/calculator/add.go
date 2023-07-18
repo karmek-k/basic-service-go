cda50e92f7eea3eb14edcab4762bbac4983d2f45
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-07-18 13:28:21 +0200

    feat: addition and subtraction logic

---
+++ pkg/calculator/add.go
@@ -0,0 +1,6 @@
+package calculator
+
+// Add gives the sum of two integers
+func Add(a, b int) int {
+	return a + b
+}
---
+++ pkg/calculator/subtract.go
@@ -0,0 +1,6 @@
+package calculator
+
+// Subtract gives the difference of two integers
+func Subtract(a, b int) int {
+	return a - b
+}
