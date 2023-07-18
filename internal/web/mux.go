a24f8a7795b146b8621a7ce6ee2d62130b10e9a0
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-07-18 20:04:20 +0200

    feat: add middlewares to the mux

--- internal/web/mux.go
+++ internal/web/mux.go
@@ -3,15 +3,20 @@ package web
 import (
 	"net/http"
 
+	"github.com/karmek-k/basic-service-go/internal/web/middleware"
 	"github.com/karmek-k/basic-service-go/internal/web/routes"
 )
 
-// CreateCalculatorMux creates a mux with registered calculator routes
-func CreateCalculatorMux() *http.ServeMux {
+// CreateCalculatorHandler creates a `http.Handler` with registered calculator routes
+func CreateCalculatorHandler() http.Handler {
 	mux := http.NewServeMux()
 	
+	handler := middleware.ArgsFromParamsMiddleware(mux)
+	// TODO fix; always returns 400
+	// handler = middleware.ValidArgsMiddleware(handler)
+
 	mux.HandleFunc("/add", routes.Add)
 	mux.HandleFunc("/subtract", routes.Subtract)
 
-	return mux
+	return handler
 }
