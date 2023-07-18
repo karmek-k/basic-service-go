68686a88334e7f81984f431e03d068aab5f67b17
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-07-18 13:50:29 +0200

    test: calculator addition tests

--- test/pkg/calculator/add_test.go
+++ test/pkg/calculator/add_test.go
@@ -3,9 +3,14 @@ package calculator_test
 import (
 	"testing"
 
+	"github.com/karmek-k/basic-service-go/pkg/calculator"
 	"github.com/karmek-k/basic-service-go/test"
 )
 
-func TestDummy(t *testing.T) {
-	test.AssertEqualInts(t, 1, 1)
+func TestAdd(t *testing.T) {
+	test.AssertEqualInts(t, 4, calculator.Add(2, 2))
+	test.AssertEqualInts(t, 0, calculator.Add(0, 0))
+	test.AssertEqualInts(t, 5, calculator.Add(7, -2))
+	test.AssertEqualInts(t, -100, calculator.Add(-50, -50))
+	test.AssertEqualInts(t, 623, calculator.Add(123, 500))
 }
