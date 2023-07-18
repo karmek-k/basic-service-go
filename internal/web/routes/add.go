6b41e42130e47592df85c2b5d308cf805ce8c376
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-07-18 20:03:55 +0200

    feat: implement routes

--- internal/web/routes/add.go
+++ internal/web/routes/add.go
@@ -1,11 +1,21 @@
 package routes
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/karmek-k/basic-service-go/internal/web/constants"
+	"github.com/karmek-k/basic-service-go/internal/web/dto"
+	"github.com/karmek-k/basic-service-go/pkg/calculator"
 )
 
-// Add adds two numbers (|n| <= 1000)
+// Add adds two numbers
 func Add(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "adding two numbers")
+	args := r.Context().Value(constants.CalculatorArgsContextKey).(*dto.CalculatorArguments)
+
+	result := calculator.Add(args.A, args.B)
+
+	w.Header().Add("Content-Type", "application/json")
+	io.WriteString(w, fmt.Sprintf("{\"result\": %d}", result))
 }
--- internal/web/routes/subtract.go
+++ internal/web/routes/subtract.go
@@ -1,11 +1,21 @@
 package routes
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/karmek-k/basic-service-go/internal/web/constants"
+	"github.com/karmek-k/basic-service-go/internal/web/dto"
+	"github.com/karmek-k/basic-service-go/pkg/calculator"
 )
 
 // Subtract subtracts two numbers (|n| <= 1000)
 func Subtract(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "subtracting two numbers")
+	args := r.Context().Value(constants.CalculatorArgsContextKey).(*dto.CalculatorArguments)
+
+	result := calculator.Subtract(args.A, args.B)
+
+	w.Header().Add("Content-Type", "application/json")
+	io.WriteString(w, fmt.Sprintf("{\"result\": %d}", result))
 }
